f2200e8a9a6636c0a8c0aa6abab7a869d024213d
jiayisu <njru8u4@gmail.com>
2017-10-14 06:40:18 +0900

    moving

--- 1.js
+++ 1.js
@@ -1,4 +1,14 @@
-big();
+//big();
+var imgs = ['img[title=0]', 'img[title=1]', 'img[title=2]', 'img[title=3]'];
+//step(0,imgs);
+normal_step();
+temer();
+
+function timer()
+{
+	normal_step();
+	setTimeout('temer',100);
+}
 
 function big()
 {
@@ -13,3 +23,41 @@ function small()
 }
 
 
+function normal_step()
+{
+	//var step = 0;
+	
+	for (var i in imgs)
+	{
+		jQuery(imgs[i]).css('top', -(jQuery(imgs[i]).height()-20)*i);
+		jQuery(imgs[i]).css('left', 100*i);
+		jQuery(imgs[i]).css('z-index', i);
+		//jQuery(imgs[(i+step)%4]).animate({'left':(i+step)%4*i*100}, 2000);
+	}
+	steps(0, imgs);
+	
+	return ;
+}
+
+function steps(step, imgs)
+{
+	//step = (step>100)?0:step;
+	step++;
+	
+	for (var i=0; i<4; i++)
+	{
+		var temp = (i+step)%4;
+		jQuery(imgs[i]).animate({'z-index':temp});
+		jQuery(imgs[i]).animate({'left':temp*100}, 2000);
+	}
+	//if (step == 4)
+		//return step;
+	
+	steps(step, imgs);
+}
+	
+	
+	
+	
+	
+	
