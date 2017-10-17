75ca8147013dc8ca91bfe92618b9050284ebf4a8
jiayisu <njru8u4@gmail.com>
2017-10-17 22:56:34 +0900

    finish first part

--- 1.js
+++ 1.js
@@ -2,7 +2,16 @@
 var imgs = ['img[title=0]', 'img[title=1]', 'img[title=2]', 'img[title=3]'];
 //step(0,imgs);
 normal_step();
+
 temer();
+
+var step = 0;
+//while (normal_step())
+{
+	
+}
+
+
 
 function timer()
 {
@@ -29,31 +38,70 @@ function normal_step()
 	
 	for (var i in imgs)
 	{
-		jQuery(imgs[i]).css('top', -(jQuery(imgs[i]).height()-20)*i);
-		jQuery(imgs[i]).css('left', 100*i);
+		jQuery(imgs[i]).css('position', 'fixed');
+		jQuery(imgs[i]).css('top', (jQuery(imgs[i]).height())*1);
+		jQuery(imgs[i]).css('left', 100*i+100);
 		jQuery(imgs[i]).css('z-index', i);
+		jQuery(imgs[i]).css('visibility', '');
 		//jQuery(imgs[(i+step)%4]).animate({'left':(i+step)%4*i*100}, 2000);
 	}
-	steps(0, imgs);
+	first_steps(0, imgs);
 	
 	return ;
 }
 
+function first_steps(step, imgs)
+{
+	for (var i=0; i<4; i++)
+	{
+		var temp = (i+step)%4;
+		
+		
+		
+		jQuery(imgs[i]).animate({'z-index':temp});
+		jQuery(imgs[i]).animate({'left':temp*100+100}, 2000);
+	}
+	
+	steps(++step, imgs);
+}
+
 function steps(step, imgs)
 {
-	//step = (step>100)?0:step;
-	step++;
+	//step++;
 	
 	for (var i=0; i<4; i++)
 	{
 		var temp = (i+step)%4;
+		if (temp == 0)
+		//	jQuery(imgs[i]).css('visibility', 'hidden');
+		//	jQuery(imgs[i]).animate({'visibility':'hidden'},100);
+		{
+			//jQuery(imgs[i]).css('left', 0);
+			
+			//jQuery(imgs[i]).animate({'z-index':temp},1000);
+			//jQuery(imgs[i]).animate({'visibility':'hidden'},1000);
+			//setTimeout(jQuery(imgs[i]).animate({'z-index':temp},0));
+			
+			jQuery(imgs[i]).fadeOut(800).animate({'left':100},0).animate({'z-index':temp},0).fadeIn(1600);
+			//jQuery(imgs[i]).css('z-index',0);
+			//setTimeout(jQuery(imgs[i]).css('z-index',0), 50);
+			//jQuery(imgs[i]).;
+			//jQuery(imgs[i]).animate('z-index',1000);
+			
+			
+			//jQuery(imgs[i]).fadeIn(1100);
+			continue;
+		}
+		
+		
+		
 		jQuery(imgs[i]).animate({'z-index':temp});
-		jQuery(imgs[i]).animate({'left':temp*100}, 2000);
+		jQuery(imgs[i]).animate({'left':temp*100+100}, 2000);
 	}
 	//if (step == 4)
 		//return step;
 	
-	steps(step, imgs);
+	steps(++step, imgs);
 }
 	
 	
